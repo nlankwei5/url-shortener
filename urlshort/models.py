aaa60630352d3ada0fe6800abc4cfd87432f1d7d
nlankwei5 <lampteyebenezer220@gmail.com>
2025-07-07 11:59:23 +0000

    Create Models for the shortener

--- urlshort/models.py
+++ urlshort/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class urlShortener(models.Model):
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=15, unique=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    clicks = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["-time_created"]
+    
+    def __str__(self):
+        return f"{self.original_url} to {self.short_url}"
+    
+    
